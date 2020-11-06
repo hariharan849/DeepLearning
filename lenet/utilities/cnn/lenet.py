171e024e481d294f8d09200c7e42edb73192e732
hariharan-m <hari.haran849@gmail.com>
2020-11-06 19:27:58 +0530

    Added Lenet

---
+++ lenet/utilities/cnn/lenet.py
@@ -0,0 +1,28 @@
+from keras.model import Sequential
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Flatten, Dense, Activation
+from keras import backend as K
+
+
+class LeNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        model = Seqential()
+
+        input_shape = (width, height, depth)
+        if K.image_data_format() == "channels_first":
+            input_shape = (depth, width, height)
+
+        model.add(Conv2d(32, (3, 3), padding="same", input_shape=input_shape))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+        model.add(Conv2d(50, (5, 5), padding="same"))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+        model.add(Flatten())
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        return model
