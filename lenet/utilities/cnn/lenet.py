6c3ffb5ec1d648780b9de83451abeebb14e713cb
hariharan-m <hari.haran849@gmail.com>
2020-11-12 15:55:02 +0530

    Update lenet.py

--- lenet/utilities/cnn/lenet.py
+++ lenet/utilities/cnn/lenet.py
@@ -7,17 +7,17 @@ from keras import backend as K
 class LeNet:
     @staticmethod
     def build(width, height, depth, classes):
-        model = Seqential()
+        model = Sequential()
 
         input_shape = (width, height, depth)
         if K.image_data_format() == "channels_first":
             input_shape = (depth, width, height)
 
-        model.add(Conv2d(32, (3, 3), padding="same", input_shape=input_shape))
+        model.add(Conv2D(32, (3, 3), padding="same", input_shape=input_shape))
         model.add(Activation("relu"))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-        model.add(Conv2d(50, (5, 5), padding="same"))
+        model.add(Conv2D(50, (5, 5), padding="same"))
         model.add(Activation("relu"))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
