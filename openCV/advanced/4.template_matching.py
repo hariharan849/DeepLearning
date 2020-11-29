714a6830f76c1fe447e958601011c35619b3dbeb
hariharan-m <hari.haran849@gmail.com>
2020-11-29 09:29:36 +0530

    updated advanced

---
+++ openCV/advanced/4.template_matching.py
@@ -0,0 +1,48 @@
+import cv2
+from google.colab.patches import cv2_imshow
+
+# The Full Image to Search
+full = cv2.imread('/content/sample_data/sammy.jpg')
+full = cv2.cvtColor(full, cv2.COLOR_BGR2RGB)
+
+
+# The Template to Match
+face= cv2.imread('/content/sample_data/sammy_face.jpg')
+face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+height, width,channels = face.shape
+
+# All the 6 methods for comparison in a list
+# Note how we are using strings, later on we'll use the eval() function to convert to function
+methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR','cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+for m in methods:
+    
+    # Create a copy of the image
+    full_copy = full.copy()
+    
+    # Get the actual function instead of the string
+    method = eval(m)
+
+    # Apply template Matching with the method
+    res = cv2.matchTemplate(full_copy,face,method)
+    
+    # Grab the Max and Min values, plus their locations
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    
+    # Set up drawing of Rectangle
+    
+    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    # Notice the coloring on the last 2 left hand side images.
+    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+        top_left = min_loc    
+    else:
+        top_left = max_loc
+        
+    # Assign the Bottom Right of the rectangle
+    bottom_right = (top_left[0] + width, top_left[1] + height)
+
+    # Draw the Red Rectangle
+    cv2.rectangle(full_copy,top_left, bottom_right, 255, 10)
+
+
+cv2_imshow(full_copy)
