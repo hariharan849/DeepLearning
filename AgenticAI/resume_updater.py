8261f525972614906cec0aed00ccb7da6bbf675c
hariharan-m <hari.haran849@gmail.com>
2025-02-28 17:47:23 +0530

    Removed secrets.toml from tracking

--- AgenticAI/resume_updater.py
+++ AgenticAI/resume_updater.py
@@ -15,14 +15,9 @@ load_dotenv()
 temp_dir = "temp_files"
 os.makedirs(temp_dir, exist_ok=True)
 
-# Retrieve LangChain API Key securely
-LANGCHAIN_API_KEY = st.secrets["LANGCHAIN_API_KEY"]
-os.environ["LANGCHAIN_API_KEY"] = LANGCHAIN_API_KEY
-GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
-os.environ("GROQ_API_KEY")
 
 # Set up LLM
-llm = ChatGroq(model_name="gemma2-9b-it", temperature=0.7, api_key=GROQ_API_KEY)
+llm = ChatGroq(model_name="gemma2-9b-it", temperature=0.7)
 
 def rebuild_resume_with_format(updated_text, formatting, file_type):
     file_path = os.path.join(temp_dir, f"updated_resume.{file_type}")
